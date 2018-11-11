46e83c9048cb6f4436791843b969d8b1dfbfae7d
Growtopia Jaw <growtopiajaw@gmail.com>
2018-11-11 12:00:56 +0800

    Update pixel.mk

--- pixel.mk
+++ pixel.mk
@@ -1,5 +1,1 @@
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk) 
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/aosp/config/common.mk)
