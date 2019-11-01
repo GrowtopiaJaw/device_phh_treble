13ddefab83d1014ab9d7bcc40ab35795b08d99fd
GrowtopiaJaw <growtopiajaw@gmail.com>
2019-11-01 11:10:43 +0800

    Correct POSP Oreo vendor directory

--- posp.mk
+++ posp.mk
@@ -1,2 +1,2 @@
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
-$(call inherit-product, device/potato/sepolicy/sepolicy.mk)
+$(call inherit-product, device/potato/sepolicy/common/sepolicy.mk)
